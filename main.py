aa7984b6aa19671e6350f938aa47763a1567cbf4
SkillBased <70569166+SkillBased@users.noreply.github.com>
2022-12-27 12:36:42 +0300

    bugfix+
    fixed typos
    added storage scan

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
     cfg = DFSConfig()
 
     # setup folders
-    run(f"mkdir -p {cfg.inputFolder} {cfg.outputFolder}, {cfg.resultsFolder}", shell=True)
+    run(f"mkdir -p {cfg.inputFolder} {cfg.outputFolder} {cfg.resultsFolder}", shell=True)
     run(f"hdfs dfs -mkdir -p {cfg.inputFolder} {cfg.outputFolder}", shell=True)
 
     exit_ = False
@@ -45,6 +45,17 @@ def main():
             except Exception:
                 print("wrong argument values, expected int, string")
             
+            possibleName = f"{query}-{depth}.result"
+            if possibleName in listdir("bot-results"):
+                print(f"some results are already stored in {possibleName}")
+                inp = ""
+                allowed = ["y", "n", "yes", "no"]
+                while inp.lower() not in allowed:
+                    inp = input("would you like to update results now (y/n): ")
+                if inp in ["n", "no"]:
+                    continue
+                print("updating now")
+
             texts = GatherByTag(query, depth)
             if len(texts) <= 5:
                 print(f"too few questions found ({len(texts)}) try increasing search depth")
@@ -68,7 +79,7 @@ def main():
             run(f"hdfs dfs -put {cfg.inputFolder}/{query}.task {cfg.inputFolder}", shell=True)
 
             # run mapred task
-            run(f'mapred streaming -input {cfg.inputFolder}/{query}.task -output {cfg.outputFolder} -mapper "python3 mapper.py" -reducer "python 3 reducer.py" -file mapper.py -file reducer.py', shell=True)
+            run(f'mapred streaming -input {cfg.inputFolder}/{query}.task -output {cfg.outputFolder} -mapper "python3 mapper.py" -reducer "python3 reducer.py" -file mapper.py -file reducer.py', shell=True)
 
             # download result
             run(f'hdfs dfs -get {cfg.outputFolder} {cfg.outputFolder}', shell=True)
@@ -77,17 +88,17 @@ def main():
             lines = []
             for filename in listdir("bot-out"):
                 if filename[:5] == "part-":
-                    with open(filename, "r") as reader:
-                        lines.extend(reader.readlines().strip().split("\n"))
+                    with open(f"bot-out/{filename}", "r") as reader:
+                        lines.extend(reader.readlines())
             
-            with open(f"{cfg.resultsFolder}/{query}.result", "w") as writer:
+            with open(f"{cfg.resultsFolder}/{query}-{depth}.result", "w") as writer:
                 for line in lines:
-                    writer.write(line + "\n")
+                    writer.write(line)
 
             print(f"task completed, look for results locally in {cfg.resultsFolder}/{query}.result")
             print(f"head of {query}.result reads:")
             for i in range(cfg.defaultSettings["trunc"]):
-                print(lines[i])
+                print(lines[i].strip())
             
 if __name__ == "__main__":
     main()
